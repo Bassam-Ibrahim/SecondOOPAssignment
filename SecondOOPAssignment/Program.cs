de942eaff6e5a7632f372e11a2ab75231b157b5a
Bassam Ibrahim Abu El-Saud <bassamb672005@gmail.com>
2026-09-13 22:57:18 +0300

    Add project files.

---
+++ SecondOOPAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+
+    public static void Main(string[] args)
+    {
+        #region Theoretical Questions
+        /*
+        Question 1
+         a)  the class is reference type and support inheritance, and the struct is value type and does not support inheritance.
+         b) the class supports inhertance so it will be easy for large projects will not write alot of code and it will be easy to maintain, but the struct is value type and does not support inheritance so it will be hard to maintain and write a lot of code.
+         
+        Question 2
+          a) Parent class is Shipment
+          b) child class is ExpressShipment
+          c) ExpressShipment inherits the TrackingCode property from Shipment.
+          d)  the child class can uses the members of parent class it will make the code more Maintainable and easier to manage
+         */
+        #endregion
+
+    }
+}
