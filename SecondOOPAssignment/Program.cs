61a820db51890469b642bad6290bca66b6f20089
Bassam Ibrahim Abu El-Saud <bassamb672005@gmail.com>
2026-09-13 22:58:35 +0300

    Theoretical Questions

--- SecondOOPAssignment/Program.cs
+++ SecondOOPAssignment/Program.cs
@@ -5,11 +5,11 @@
     {
         #region Theoretical Questions
         /*
-        Question 1
+        Question  Number 1
          a)  the class is reference type and support inheritance, and the struct is value type and does not support inheritance.
          b) the class supports inhertance so it will be easy for large projects will not write alot of code and it will be easy to maintain, but the struct is value type and does not support inheritance so it will be hard to maintain and write a lot of code.
          
-        Question 2
+        Question  Number 2
           a) Parent class is Shipment
           b) child class is ExpressShipment
           c) ExpressShipment inherits the TrackingCode property from Shipment.
